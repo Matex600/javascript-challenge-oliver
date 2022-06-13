1d57d99e17f08906fd6215b002f06f2f8af42d74
Matex600 <mat.leks@gmail.com>
2022-06-13 19:10:03 +0000

    Create javascript to calculate 10% commission

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+let calculateBtn = document.getElementById('calculate');
+
+calculate.addEventListener('click', function(){
+    let initialCharge = document.getElementById('speaker').value;
+    let percentageCommision = document.getElementById('percentage-commision').value;
+
+    
+    let organiserCommission = document.getElementById('commission-amount').value = (initialCharge / 100) * percentageCommision;
+    document.getElementById('final-charge').value = parseFloat(initialCharge) + parseFloat(organiserCommission);
+    e.preventDefault();
+})
